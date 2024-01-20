b0c426b64956d63d88d147bae3efc7d939f6fcab
Eduardo P. <edtvports@gmail.com>
2024-01-19 21:05:46 -0300

    Basic home design

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,7 +9,7 @@
             <NuxtLink to="/projects">Projects</NuxtLink>
         </nav>
     </header>
-    <main class="mx-6 md:mx-24 lg:mx-32 2xl:max-w-[70%] 2xl:mx-[15%]">
+    <main class="mx-6 md:mx-24 lg:mx-32 2xl:max-w-[70%] 2xl:mx-[15%] min-h-full">
         <slot />
     </main>
 </template>
