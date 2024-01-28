cfc746b71da49122e8054fde68709f84667ac55d
Eduardo P. <edtvports@gmail.com>
2024-01-28 17:11:46 -0300

    Finished sales pitch (added image)

--- components/home/Pitch.vue
+++ components/home/Pitch.vue
@@ -1,5 +1,7 @@
 <template>
     <section class="w-fit flex flex-col justify-between h-[95vh] mx-auto text-center">
+        <img class="absolute top-0 left-0 w-full h-full object-cover brightness-50 -z-10" src="~/assets/images/pexels-sergei-starostin-6466141.jpg" alt="sala cheia de servidores">
+
         <div class="my-auto">
             <h1 class="whitespace-nowrap font-extrabold" style="font-size: 10vmin;">ALPHA JUNIOR</h1>
             <div class="flex flex-col w-full justify-center mt-4 text-xl text-start font-extrabold gap-6" style="font-size: 5vmin;">
