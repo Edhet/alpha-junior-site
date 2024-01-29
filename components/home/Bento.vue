1d1ab6570e9839b243b08eb8f0769fd52036c368
Eduardo P. <edtvports@gmail.com>
2024-01-29 19:35:14 -0300

    Changed border color in bento boxes

--- components/home/Bento.vue
+++ components/home/Bento.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="my-40">
         <div>
-            <div class="p-6 m-1 md:m-3 border border-zinc-900 rounded-lg">
+            <div class="p-6 m-1 md:m-3 border border-white/20 rounded-lg">
                 <div class="flex">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-info-lg my-auto" viewBox="0 0 16 16">
                         <path d="m9.708 6.075-3.024.379-.108.502.595.108c.387.093.464.232.38.619l-.975 4.577c-.255 1.183.14 1.74 1.067 1.74.72 0 1.554-.332 1.933-.789l.116-.549c-.263.232-.65.325-.905.325-.363 0-.494-.255-.402-.704zm.091-2.755a1.32 1.32 0 1 1-2.64 0 1.32 1.32 0 0 1 2.64 0" />
@@ -15,7 +15,7 @@
             </div>
         </div>
         <div class="flex flex-col md:grid md:grid-cols-3">
-            <div class="p-6 m-1 md:m-3 border border-zinc-900 rounded-lg">
+            <div class="p-6 m-1 md:m-3 border border-white/20 rounded-lg">
                 <div class="flex">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-stars my-auto" viewBox="0 0 16 16">
                         <path d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.73 1.73 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.73 1.73 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.73 1.73 0 0 0 3.407 2.31zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.16 1.16 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.16 1.16 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732z" />
@@ -24,7 +24,7 @@
                 </div>
                 <p class="font-light italic">Capacitar nossos funcionários através da entrega de um serviço de qualidade na área de TI.</p>
             </div>
-            <div class="p-6 m-1 md:m-3 border border-zinc-900 rounded-lg">
+            <div class="p-6 m-1 md:m-3 border border-white/20 rounded-lg">
                 <div class="flex">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-eye-fill my-auto" viewBox="0 0 16 16">
                         <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0" />
@@ -34,7 +34,7 @@
                 </div>
                 <p class="font-light italic">Se tornar a referência em construção de soluções de TI das empresas juniores.</p>
             </div>
-            <div class="p-6 m-1 md:m-3 border border-zinc-900 rounded-lg">
+            <div class="p-6 m-1 md:m-3 border border-white/20 rounded-lg">
                 <div class="flex">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-bank2 my-auto" viewBox="0 0 16 16">
                         <path d="M8.277.084a.5.5 0 0 0-.554 0l-7.5 5A.5.5 0 0 0 .5 6h1.875v7H1.5a.5.5 0 0 0 0 1h13a.5.5 0 1 0 0-1h-.875V6H15.5a.5.5 0 0 0 .277-.916zM12.375 6v7h-1.25V6zm-2.5 0v7h-1.25V6zm-2.5 0v7h-1.25V6zm-2.5 0v7h-1.25V6zM8 4a1 1 0 1 1 0-2 1 1 0 0 1 0 2M.5 15a.5.5 0 0 0 0 1h15a.5.5 0 1 0 0-1z" />
@@ -45,7 +45,7 @@
             </div>
         </div>
         <div>
-            <div class="flex flex-col md:flex-row col-span-2 gap-4 p-6 m-1 md:m-3 border border-zinc-900 rounded-lg">
+            <div class="flex flex-col md:flex-row col-span-2 gap-4 p-6 m-1 md:m-3 border border-white/20 rounded-lg">
                 <div class="mr-2">
                     <div class="flex">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-people-fill my-auto" viewBox="0 0 16 16">
